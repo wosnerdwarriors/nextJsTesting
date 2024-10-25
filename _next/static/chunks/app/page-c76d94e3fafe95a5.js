ea75a1f4f5d8d3edfa0e66fdc61339e66139f779
m-bobi <m-bobi@users.noreply.github.com>
2024-10-25 11:28:58 +0000

    deploy: 918820cab896bef2773e403ad527bc156bb6588a

---
+++ _next/static/chunks/app/page-c76d94e3fafe95a5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{7714:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8173,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[173,130,215,744],()=>s(7714)),_N_E=e.O()}]);
